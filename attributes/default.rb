a9a0cc3f3d0b40fcecc423d757e79358cd817812
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2019-12-18 12:37:38 +0800

    change default customer name to 'ACME'

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['nginx']['rhel_version'] = 7
 default['nginx']['package_name'] = "nginx"
 default['nginx']['port'] = 8080
-default['customer'] = "BCA"
+default['customer'] = "ACME"
 default['webserver'] ='generic'
